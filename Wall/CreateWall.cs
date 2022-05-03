292b058d596e90877b612ac56a3048e1f72c66f4
João Matias <96756580+jmatiasmn@users.noreply.github.com>
2022-05-03 17:56:02 -0300

    Update CreateWall.cs

--- Wall/CreateWall.cs
+++ Wall/CreateWall.cs
@@ -1,19 +1,20 @@
 public void CreateWall()
-		{
-			Document doc = this.ActiveUIDocument.Document;
-			XYZ start = new XYZ(0, 0, 0);
-    		XYZ end = new XYZ(10, 10, 0);
-    		Line geomLine = Line.CreateBound(start, end);
-    		ElementId level = new FilteredElementCollector(doc)
-    			.OfCategory(BuiltInCategory.OST_Levels)
-    			.WhereElementIsNotElementType()
-    			.FirstElement().Id;
+{
+	Document doc = this.ActiveUIDocument.Document;
+	XYZ start = new XYZ(0, 0, 0);
+	XYZ end = new XYZ(10, 10, 0);
+	Line geomLine = Line.CreateBound(start, end);
+	ElementId level = new FilteredElementCollector(doc)
+	.OfCategory(BuiltInCategory.OST_Levels)
+	.WhereElementIsNotElementType()
+	.FirstElement().Id;
 
-			using (Transaction t = new Transaction(doc,"Create Wall")) 
-			{
-				t.Start();
-				
-				Wall wall = Wall.Create(doc,geomLine,level,false);
-				t.Commit();
-				
-			}
+	using (Transaction t = new Transaction(doc,"Create Wall")) 
+	{
+		t.Start();
+
+		Wall wall = Wall.Create(doc,geomLine,level,false);
+		t.Commit();
+
+	}
+}
