e85db7fc7750a26b971d186830229df2457eb6d9
João Matias <96756580+jmatiasmn@users.noreply.github.com>
2022-05-03 17:55:17 -0300

    Create wall in C#

---
+++ Wall/CreateWall.cs
@@ -0,0 +1,19 @@
+public void CreateWall()
+		{
+			Document doc = this.ActiveUIDocument.Document;
+			XYZ start = new XYZ(0, 0, 0);
+    		XYZ end = new XYZ(10, 10, 0);
+    		Line geomLine = Line.CreateBound(start, end);
+    		ElementId level = new FilteredElementCollector(doc)
+    			.OfCategory(BuiltInCategory.OST_Levels)
+    			.WhereElementIsNotElementType()
+    			.FirstElement().Id;
+
+			using (Transaction t = new Transaction(doc,"Create Wall")) 
+			{
+				t.Start();
+				
+				Wall wall = Wall.Create(doc,geomLine,level,false);
+				t.Commit();
+				
+			}
